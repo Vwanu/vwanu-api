51e6b77d9fd8ea00ee6f1085f923fa5d57485ddf
Wadson Vaval <vavalson@hotmail.com>
2025-04-06 10:45:49 -0400

    Stripped away client

--- client/src/features/external/externalSlice.js
+++
@@ -1,4 +0,0 @@
-import { useFetch } from "../../lib/react-query";
-
-export const useLinkPreview = (queryKey, enabled = false, url) =>
-  useFetch(queryKey, enabled, `https://get-link-preview.herokuapp.com/?url=${url}`);
--- client/src/lib/appendStreamToPage.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-unused-vars */
-
-/**
- * THis function takes an stream and a media and adds it to the docs
- */
-export default function appendStreamToPage(media: HTMLMediaElement, stream: STREAM, errCb?: (e: ERROR) => {}): void;
