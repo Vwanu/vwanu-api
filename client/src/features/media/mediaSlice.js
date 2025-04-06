ef87e734845f1128c0a819dbc856f4b35d119596
Wadson Vaval <vavalson@hotmail.com>
2025-04-06 10:45:49 -0400

    Stripped away client

--- client/src/features/media/mediaSlice.js
+++
@@ -1,6 +0,0 @@
-import { api } from "../../lib/api";
-import { useDelete } from "../../lib/react-query";
-
-export const uploadImage = (data) => api.post(`/medias`, data);
-
-export const useDeleteImage = (queryKey) => useDelete(queryKey, `/medias`, (undefined, undefined));
