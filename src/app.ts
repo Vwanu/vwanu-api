6c51e158fda4b4d9d7f11893f112dcb2730a8af1
Wadson Vaval <vavalson@hotmail.com>
2025-05-17 18:52:48 -0400

    Added all services

--- src/app.ts
+++ src/app.ts
@@ -1,65 +1,49 @@
 /* eslint-disable no-unused-vars */
-// import cors from 'cors';
-// import helmet from 'helmet';
-// import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 import express from '@feathersjs/express';
-// // import socketio from '@feathersjs/socketio';
-// import methodOverride from 'method-override';
-// import configuration from '@feathersjs/configuration';
-// import { Request, Response, NextFunction } from 'express';
-// import feathers from '@feathersjs/feathers';
+import socketio from '@feathersjs/socketio';
+import methodOverride from 'method-override';
+import configuration from '@feathersjs/configuration';
+import { Request, Response, NextFunction } from 'express';
+import feathers from '@feathersjs/feathers';
 
 // /** Custom dependencies */
-// // import channels from './channels';
-// // import database from './database';
-// // import services from './services';
-// // import sequelize from './sequelize';
-// // import middleware from './middleware';
-// import common from './lib/utils/common';
-// import { Application } from './declarations';
-// import RequestBody from './middleware/RequestBody';
+import channels from './channels';
+import database from './database';
+import services from './services';
+import sequelize from './sequelize';
+import middleware from './middleware';
+import common from './lib/utils/common';
+import { Application } from './declarations';
+import RequestBody from './middleware/RequestBody';
 
 dotenv.config();
-// const { sendErrorResponse } = common;
+const { sendErrorResponse } = common;
 
-// const app = express(feathers());
-// app.configure(configuration());
-// app.use(express.json());
+const app = express(feathers());
+app.configure(configuration());
+app.use(express.json());
 
-// app.use(cors());
-// app.use(helmet());
-// app.use(RequestBody);
-// app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' }));
-// app.use(methodOverride('_method') as any);
-// app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(RequestBody);
+app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' }));
+app.use(methodOverride('_method') as any);
+app.use(express.urlencoded({ extended: true }));
 
-// app.configure(express.rest());
-// // app.configure(socketio());
-// // app.configure(sequelize);
+app.configure(express.rest());
+app.configure(socketio());
+app.configure(sequelize);
 
-// // app.configure(middleware);
-// // app.configure(channels);
-// // app.configure(database);
-// // app.get('startSequelize')();
-// // app.configure(services);
+app.configure(middleware);
+app.configure(channels);
+app.configure(database);
+app.get('startSequelize')();
+app.configure(services);
 
-// app.use('/health', (req: Request, res: Response) => {
-//   res.status(200).json({ status: 'healthy' });
-// });
-// app.use(express.notFound());
-// app.use(express.errorHandler({ logger: console } as any));
-
-// app.use((err: Error | any, req: Request, res: Response, next: NextFunction) =>
-//   res
-//     .status(err.status || err.code || 500)
-//     .json({ error: err.message || 'Internal Server Error' })
-// );
-
-// export default app;
-
-
-const app = express();
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'healthy', dummy: true, version: '3.0' });
@@ -81,10 +65,14 @@ app.get('/secret', (req, res) => {
   res.status(200).json({ status: 'healthy', dummy: true, version: '3.0', rds_credential });
 });
 
-// const PORT = 3000;
-// app.listen(PORT, '0.0.0.0', () => {
-//   console.log(`Dummy app listening on port ${PORT}`);
-// });
+app.use(express.notFound());
+app.use(express.errorHandler({ logger: console } as any));
+
+app.use((err: Error | any, req: Request, res: Response, next: NextFunction) =>
+  res
+    .status(err.status || err.code || 500)
+    .json({ error: err.message || 'Internal Server Error' })
+);
 
 export default app;
 
