d5bde942b885b5ebabed0eb8093ce1c288756d3c
Wadson Vaval <vavalson@hotmail.com>
2025-05-18 13:55:27 -0400

    All services running

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,44 +1,36 @@
 import app from '../app';
-// import Logger from '../lib/utils/logger';
-// import {
-  // ApiConfigurationType,
-  // API_CONFIG_SCHEMA,
-// } from '../schema/serverConf.schema';
-// import helper from './sever_helper';
+import Logger from '../lib/utils/logger';
+import {
+  ApiConfigurationType,
+  API_CONFIG_SCHEMA,
+} from '../schema/serverConf.schema';
+import helper from './sever_helper';
 
-// const API_CONFIGURATION: ApiConfigurationType = app.get('API_CONFIGURATION');
+const API_CONFIGURATION: ApiConfigurationType = app.get('API_CONFIGURATION');
 
-const port = 3000;
-const Logger = console;
+// helper.envConfigurationCheck();
 
-// Configure a middleware for 404s and the error handler
+if (API_CONFIG_SCHEMA.safeParse(API_CONFIGURATION)) {
+  const {host} = API_CONFIGURATION;
+  const port = helper.normalizePort(API_CONFIGURATION.port);
+  // @ts-ignore
+  const server = app.listen(port, host);
 
-app.listen(port, '0.0.0.0', () => {
-  Logger.log(`Server is running on port ${port}`);
+  server.on('error', (err) => {helper.onError(err, port)});
+  server.on('listening', () => {
+  app
+    .get('sequelizeSync')
+    .then(() => {
+    helper.onListening(server, host?? '0.0.0.0')})
+  .catch((error) => {
+    Logger.error(error);
+    process.exit(1);
+  });
 });
 
-/*  server.on('error', (err) => {
-  helper.onError(err, port);
-});
+} else {
+  Logger.error('[API configuration] Missing required environment variables');
+  process.exit(1);
+}
 
-server.on('listening', () => {
-  // helper.envConfigurationCheck();
-  // app
-  //   .get('sequelizeSync')
-  //   .then(() => {
-  helper.onListening(server, '0.0.0.0');
-  // })
-  // .catch((error) => {
-  //   Logger.error(error);
-  //   process.exit(1);
-  // });
-});
 
-// if (API_CONFIG_SCHEMA.parse(API_CONFIGURATION)) {
-//   port = helper.normalizePort(API_CONFIGURATION.port);
-// } else {
-//   Logger.error('[API configuration] Missing required environment variables');
-//   process.exit(1);
-// }
-
-*/
