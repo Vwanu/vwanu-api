ad6b72e898a550bdbcfa8a0c8b84092aa16ba9ee
Wadson Vaval <vavalson@hotmail.com>
2025-07-06 03:17:03 +0000

    remove terraform

--- terraform/modules/route53/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "hosted_zone_id" {
-  description = "Route 53 hosted zone ID"
-  value       = data.aws_route53_zone.main.zone_id
-}
-
-output "api_fqdn" {
-  description = "API fully qualified domain name"
-  value       = aws_route53_record.api.fqdn
-} 
