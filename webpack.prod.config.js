5337a4971946145124863f843002e2be1ca33cf2
Wadson Vaval <vavalson@hotmail.com>
2025-07-07 22:32:45 -0400

    add working webpack

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -48,8 +48,8 @@ module.exports = {
     },
   },
   output: {
-    path: path.resolve(__dirname, '.build'),
-    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
     chunkFilename: '[name].[chunkhash].js',
     clean: true,
   },
