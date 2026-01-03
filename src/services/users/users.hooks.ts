bb64354de156b36239995f228c4dbbbc16fd5b6d
Wadson Vaval <vavalson@hotmail>
2026-01-03 00:22:16 +0000

    add hot fix

--- src/services/users/users.hooks.ts
+++ src/services/users/users.hooks.ts
@@ -1,6 +1,6 @@
 import commonHooks from 'feathers-hooks-common';
 import * as local from '@feathersjs/authentication-local';
-import { HookContext } from '@feathersjs/feathers';
+// import { HookContext } from '@feathersjs/feathers';
 
 import isSelf from '../../Hooks/isSelf.hook';
 import updateTsVector from './hook/updateTsVector';
@@ -10,19 +10,19 @@ import includeFriendshipStatus from './hook/includeFriendshipStatus';
 const { protect } = local.hooks;
 const protectKeys = protect(...['search_vector']);
 
-const Addvisitor= async (context: HookContext): Promise<HookContext> => {
-  if (!context?.result ) return context;
-    await context.app.service('notifications').create({
-      fromUserId: context.params.User.id,
-      userId: context.result.id,
-      message: 'Visited your profile',
-      type: 'direct',
-      entityName: 'User',
-      entityId: context.params.User.id,
-      notificationType: 'visit',
-    });
-  return context;
-};
+// const Addvisitor= async (context: HookContext): Promise<HookContext> => {
+//   if (!context?.result ) return context;
+//     await context.app.service('notifications').create({
+//       fromUserId: context.params.User.id,
+//       userId: context.result.id,
+//       message: 'Visited your profile',
+//       type: 'direct',
+//       entityName: 'User',
+//       entityId: context.params.User.id,
+//       notificationType: 'visit',
+//     });
+//   return context;
+// };
 
 
 const hooks = {
