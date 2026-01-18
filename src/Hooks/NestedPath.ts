bd107a4dd68f381f2e754d3817582fadf069073d
Wadson Vaval <vavalson@hotmail>
2026-01-18 13:08:42 +0000

    remove debug messages in Nestedpath

--- src/Hooks/NestedPath.ts
+++ src/Hooks/NestedPath.ts
@@ -1,8 +1,6 @@
     import { HookContext } from "@feathersjs/feathers";
 
     export default (context: HookContext) => {
-        console.log('NestedPath Hook executed');
-        console.log('Route params:', context.params.route);
         context.params.query = {
           ...context.params.query,
           ...context.params.route
